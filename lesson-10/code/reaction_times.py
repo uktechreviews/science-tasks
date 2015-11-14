854b704f759ecd23b21ea7cf4ff4ee348544cd02
uktechreviews <spencerorgan1@me.com>
2015-11-14 09:44:30 +0000

    added traffic lights

--- lesson-10/code/reaction_times.py
+++ lesson-10/code/reaction_times.py
@@ -1,27 +1,45 @@
+#Investigating Reaction times on the motorway
+#Spencer Organ - KESH Academy
+#You will need to plug in a pair of earphones for the audio test
+
+from sense_hat import SenseHat
+sense = SenseHat()
 import time
+import sys
 import random
 
-print "Reaction time game" print "Python Project 8"
-print "" 
-  
+sense.clear(255,255,255)
 
-print "The green light will stay on for a random amount of time between 1 and 10 seconds" 
-print "It will then swap to the red light"
-print "As soon as it changes hit the switch"
-print "We start in 5 seconds"
+b = (0,0,0)
+r = (255,0,0)
+g = (0,255,0)
+w = (255,255,255)
+
+red_on = [
+b,b,w,w,w,w,b,b,
+b,b,w,r,r,w,b,b,
+b,b,w,r,r,w,b,b,
+b,b,w,w,w,w,b,b,
+b,b,w,w,w,w,b,b,
+b,b,w,b,b,w,b,b,
+b,b,w,b,b,w,b,b,
+b,b,w,w,w,w,b,b
+]
+
+green_on = [
+b,b,w,w,w,w,b,b,
+b,b,w,b,b,w,b,b,
+b,b,w,b,b,w,b,b,
+b,b,w,w,w,w,b,b,
+b,b,w,w,w,w,b,b,
+b,b,w,g,g,w,b,b,
+b,b,w,g,g,w,b,b,
+b,b,w,w,w,w,b,b
+]
+
+sense.set_pixels(red_on)
 time.sleep(5)
+sense.clear(0,0,0)
+sense.set_pixels(green_on)
+sense.clear(0,0,0)
 
-try:
-	while True:
-		if (GPIO.input(12) ==1): 
-			end = time.time()
-			print "you pressed the button" elapsed = end - start
-			print "and it took "
-			print round(elapsed,2)
-			print "Try to beat that next time" 
-			GPIO.cleanup() 
-			
-			break
-		else:
-			
-except KeyboardInterrupt: print "game over"
